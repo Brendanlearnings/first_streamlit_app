4861892a23f4ae5c08258dd3c55ab3bf6a73140f
Brendan De Villiers <106869469+Brendanlearnings@users.noreply.github.com>
2022-06-08 17:34:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruit_normal)
 # Query our account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur = execute("select current_user(), current_account(), current_region()")
+my_cur = execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION();")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake")
 streamlit.text(my_data_row)
