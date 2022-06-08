6e81ad6677d71308beff754566af733ba1a5a620
Brendan De Villiers <106869469+Brendanlearnings@users.noreply.github.com>
2022-06-08 17:48:29 +0200

    Update streamlit_app.py
    Added additional Snowflake executable

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_row)
 # Allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 streamlit.write("Thanks for adding ", add_my_fruit)
+
+# This wont work - for showcasing purposes. 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
